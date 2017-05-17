e3a6edeb2cbfc05337546fe99da5ffd21224905b
Nikita Shalimov <nshalimov@berkeley.edu>
2017-05-17 11:24:25 -0700

    Remove note actions from window

--- frontend/simply_note.jsx
+++ frontend/simply_note.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import * as NoteActions from './actions/notes_actions';
-window.NoteActions = NoteActions;
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store;
