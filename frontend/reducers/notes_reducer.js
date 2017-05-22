fbb36e1383392fd63f52841d0101a95ac58f0b36
Nikita Shalimov <nshalimov@berkeley.edu>
2017-05-22 15:25:03 -0700

    Just going to filter on frontend

--- frontend/reducers/notes_reducer.js
+++ frontend/reducers/notes_reducer.js
@@ -7,21 +7,15 @@ import { merge } from 'lodash';
 
 const notesReducer = (state = {}, action) => {
   Object.freeze(state);
-  const newState = merge({}, state);
   switch (action.type) {
     case RECEIVE_ALL_NOTES:
       return action.notes;
     case RECEIVE_SINGLE_NOTE:
       const trimmedNote = merge({}, action.note);
       delete trimmedNote.body;
-
-      if(state[trimmedNote.id] && state[trimmedNote.id].notebook_id !== trimmedNote.notebook_id) {
-        delete newState[trimmedNote.id];
-        return newState;
-      } else {
-        return merge({}, state, {[trimmedNote.id]: trimmedNote});
-      }
+      return merge({}, state, {[trimmedNote.id]: trimmedNote});
     case REMOVE_NOTE:
+    const newState = merge({}, state);
       delete newState[action.id];
       return newState;
     default:
