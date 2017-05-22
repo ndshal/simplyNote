d3f5585060cf9b3120f8b807b2a1de1b8217af89
Nikita Shalimov <nshalimov@berkeley.edu>
2017-05-22 11:28:12 -0700

    Add notebooks reducer

--- frontend/reducers/notebooks_reducer.js
+++ frontend/reducers/notebooks_reducer.js
@@ -8,6 +8,14 @@ import { merge } from 'lodash';
 const notebooksReducer = (state={}, action) => {
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_ALL_NOTEBOOKS:
+      return action.notebooks;
+    case RECEIVE_SINGLE_NOTEBOOK:
+      return merge({}, state, {[action.notebok.id]: action.notebook});
+    case REMOVE_NOTEBOOK:
+      const newState = merge({}, state);
+      delete newState[action.id];
+      return newState;
     default:
       return state;
   }
