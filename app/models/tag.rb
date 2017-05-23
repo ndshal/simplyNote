24d404eadec4caef75b1edb07d8ac6d594bcceef
Nikita Shalimov <nshalimov@berkeley.edu>
2017-05-23 10:38:01 -0700

    Add author to tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,8 @@
 class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :author, presence: true
+
+  belongs_to :author, class_name: :User
   has_many :tagging
   has_many :notes, through: :tagging
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523172646) do
+ActiveRecord::Schema.define(version: 20170523173411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170523172646) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "author_id",  null: false
     t.index ["name"], name: "index_tags_on_name", using: :btree
   end
 
