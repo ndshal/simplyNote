486af586f545e3624fdab7c921091458edc15d68
Nikita Shalimov <nshalimov@berkeley.edu>
2017-05-23 15:55:44 -0700

    Rename notebook title to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523173411) do
+ActiveRecord::Schema.define(version: 20170523225112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "notebooks", force: :cascade do |t|
-    t.string   "title",       null: false
-    t.integer  "author_id",   null: false
+    t.string   "name",                        null: false
+    t.integer  "author_id",                   null: false
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "is_default",  default: false
     t.index ["author_id"], name: "index_notebooks_on_author_id", using: :btree
   end
 
