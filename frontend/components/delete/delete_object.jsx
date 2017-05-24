3ba2c5bc48295cb9893d8457f633f80cb56440bf
Nikita Shalimov <nshalimov@berkeley.edu>
2017-05-24 16:42:44 -0700

    Style delete form

--- frontend/components/delete/delete_object.jsx
+++ frontend/components/delete/delete_object.jsx
@@ -33,8 +33,8 @@ class DeleteObject extends Component {
           </span>
         </header>
 
-        <div>
-          Are you sure you want to delete <span>{name}</span>?
+        <div className='message'>
+          Are you sure you want to delete <span className='subject'>{name}</span>?
         </div>
 
         <div className='object-form-btns'>
